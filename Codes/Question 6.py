71dfd62dabe20eb126808d04381d16f113651185
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-08 22:03:46 +0530

    Add files via upload

---
+++ Codes/Question 6.py
@@ -0,0 +1,7 @@
+#Q = https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users['name'] = users['name'].str.capitalize()
+    return users
