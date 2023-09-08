dafd4a8f7831e70314a2dce6be399d68d1b8221d
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-08 22:24:12 +0530

    Question 6.py

--- Codes/Question 6.py
+++ Codes/Question 6.py
@@ -4,4 +4,5 @@ import pandas as pd
 
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
     users['name'] = users['name'].str.capitalize()
+    users = users.sort_values(by = "user_id")
     return users
