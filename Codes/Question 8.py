426c12cd0a4ef55dff2508458d2bafbe29e8bb70
Saurabh Diwane <119566028+Saurabhdiwane7@users.noreply.github.com>
2023-09-09 00:46:04 +0530

    code

--- Codes/Question 8.py
+++ Codes/Question 8.py
@@ -1,7 +1,7 @@
 #Q - https://leetcode.com/problems/delete-duplicate-emails/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
-
 import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
-    df = person.drop_duplicates(subset="email",keep ="first",inplace =True)
-    return df
+    person.sort_values(by = "id",inplace =True)
+    person.drop_duplicates(subset= "email", inplace=True)
+    
